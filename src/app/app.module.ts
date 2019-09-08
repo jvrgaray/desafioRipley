237d7b4f292bccddd0ad64b897f22dcc5ddc10ef
Javier Garay <javier.garay@2brains.cl>
2019-09-08 18:13:05 -0300

    estructura inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LogiComponent } from './components/users/logi/logi.component';
+import { RegisterComponent } from './components/users/register/register.component';
+import { Page404Component } from './components/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ModalComponent,
+    NavbarComponent,
+    LogiComponent,
+    RegisterComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
